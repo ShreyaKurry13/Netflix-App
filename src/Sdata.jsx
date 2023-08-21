995f3491aa22bc762b0f6bfc1b7d16e515547983
ShreyaKurry13 <109572940+ShreyaKurry13@users.noreply.github.com>
2023-08-21 23:28:42 +0530

    netflix app

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata = [
+    {
+      imgscr:"https://wallpapercave.com/dwp1x/wp4056410.jpg",
+      title : "A original Netflix series",
+      sname :"DARK",
+      link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+      imgscr:"https://wallpapercave.com/dwp1x/wp6754493.jpg",
+      title : "A original Netflix series",
+      sname:"EXTRA CURRICULAR",
+      link:"https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+      imgscr:"https://wallpapercave.com/dwp1x/wp11827144.png",
+      title : "A original Netflix series",
+      sname:"STRANGER THINGS",
+      link:"https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgscr:"https://wallpapercave.com/dwp1x/wp12630943.jpg",
+        title : "A original Netflix series",
+        sname:"VAMPIRE DIARIES",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgscr:"https://wallpapercave.com/dwp1x/wp7632608.jpg",
+        title : "A original Netflix series",
+        sname:"MY FIRST FIRST LOVE",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+    }
+]
+export default Sdata;                   
